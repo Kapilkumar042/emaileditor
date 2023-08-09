d3b0c82963017f4aaf048585ce9d851ce9afb911
kapilkumar009 <kapil.kumar@vikasit.com>
2023-08-09 17:11:36 +0530

    apply font family in custom header

--- demo/src/components/Header/CustomHeader.tsx
+++ demo/src/components/Header/CustomHeader.tsx
@@ -11,23 +11,25 @@ const CustomHeader: FC = () => {
         <button className='border px-2 py-2 rounded-sm hover:bg-[#F4F4F5]'>
         <IconDoubleLeft />
         </button>
-        <span className="text-[#09090B]  font-sans text-[1rem]">Template for launch of SendMillion...</span>
+        <span className="text-[#09090B]  font-inter font-medium text-[1.25rem]">Template for launch of SendMillion...</span>
         <div className='flex items-center gap-2 hover:bg-[#F4F4F5] px-2 py-1 rounded-md'>
           <span>
             <IconClockCircle />
           </span>
-          <span className="text-xs">Last save 11:30 AM</span>
+          <span className="text-xs font-inter font-normal text-[0.775rem]">Last save 11:30 AM</span>
         </div>
       </div>
       <div className='flex gap-4'>
         <div>
           <Button type='secondary' icon={<IconEye />}>
-            <span className='text-[12px]'>Preivew & Test</span>
+            <span className='font-inter font-medium text-[0.825rem]'>Preivew & Test</span>
           </Button>
         </div>
         <div>
         <ButtonGroup>
-        <Button type='primary'>Save & Publish</Button>
+        <Button type='primary'>
+          <span className='font-inter font-normal text-[0.875rem]'>Save & Publish</span>
+        </Button>
         <Button type='primary' icon={<IconMoreVertical />} />
       </ButtonGroup>
         </div>
