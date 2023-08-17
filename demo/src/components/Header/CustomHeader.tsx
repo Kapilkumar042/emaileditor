ce282185c91ce6b28036e3ac13f5226109fd8ece
Akash-kpt <akash.tyagi@vikasit.com>
2023-08-17 18:47:35 +0530

    commit header back button functionality

--- demo/src/components/Header/CustomHeader.tsx
+++ demo/src/components/Header/CustomHeader.tsx
@@ -1,14 +1,15 @@
 import { Button } from '@arco-design/web-react';
 import { IconClockCircle, IconDoubleLeft, IconEye, IconMoreVertical } from '@arco-design/web-react/icon';
 import React, { FC } from 'react';
-
+import { useHistory } from 'react-router-dom';
 
 const CustomHeader: FC = () => {
+  const history = useHistory();
   const ButtonGroup = Button.Group;
   return (
     <header className='flex justify-between w-full h-16 px-3 py-4'>
       <div className='flex items-center gap-4'>
-        <button className='border px-2 py-2 rounded-sm hover:bg-[#F4F4F5]'>
+        <button className='border px-2 py-2 rounded-sm hover:bg-[#F4F4F5]' onClick={() => { history.push('/'); }}>
           <IconDoubleLeft />
         </button>
         <span className="text-[#09090B]  font-inter font-medium text-[1.25rem]">Template for launch of SendMillion...</span>
