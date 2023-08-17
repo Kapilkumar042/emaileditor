2d49fbee841cfc76a3e35efc05c7990063d1883b
kapilkumar009 <kapil.kumar@vikasit.com>
2023-08-17 09:39:09 +0530

    remove layer section and add layout section

--- packages/vikasit-email-extensions/src/EditPanel/Blocks/index.tsx
+++ packages/vikasit-email-extensions/src/EditPanel/Blocks/index.tsx
@@ -11,10 +11,8 @@ export function Blocks() {
   const { categories } = useExtensionProps();
 
   const defaultActiveKey = useMemo(
-    () => [
-      ...categories.filter((item) => item.active).map((item) => item.label),
-    ],
-    [categories]
+    () => [...categories.filter(item => item.active).map(item => item.label)],
+    [categories],
   );
   return (
     <Collapse
@@ -24,27 +22,28 @@ export function Blocks() {
       {categories.map((cat, index) => {
         if (cat.displayType === 'column') {
           return (
-            <Collapse.Item
-              key={index}
-              contentStyle={{ padding: '0px 20px' }}
-              name={cat.label}
-              header={cat.label}
-            >
-              <Space direction='vertical'>
-                <div />
-              </Space>
+            // <Collapse.Item
+            //   key={index}
+            //   contentStyle={{ padding: '0px 20px' }}
+            //   name={cat.label}
+            //   header={cat.label}
+            // >
+            //   <Space direction='vertical'>
+            //     <div />
+            //   </Space>
 
-              {cat.blocks.map((item) => (
-                <LayoutItem
-                  key={item.title}
-                  title={item.title || ''}
-                  columns={item.payload}
-                />
-              ))}
-              <Space direction='vertical'>
-                <div />
-              </Space>
-            </Collapse.Item>
+            //   {cat.blocks.map((item) => (
+            //     <LayoutItem
+            //       key={item.title}
+            //       title={item.title || ''}
+            //       columns={item.payload}
+            //     />
+            //   ))}
+            //   <Space direction='vertical'>
+            //     <div />
+            //   </Space>
+            // </Collapse.Item>
+            <h1 className='hidden'></h1>
           );
         }
 
@@ -73,7 +72,12 @@ export function Blocks() {
           >
             <Grid.Row>
               {cat.blocks.map((item, index) => {
-                return <BlockItem key={index} {...(item as any)} />;
+                return (
+                  <BlockItem
+                    key={index}
+                    {...(item as any)}
+                  />
+                );
               })}
             </Grid.Row>
           </Collapse.Item>
@@ -98,7 +102,10 @@ function BlockItem({
 
   return (
     <div className={styles.blockItem}>
-      <BlockAvatarWrapper type={type} payload={payload}>
+      <BlockAvatarWrapper
+        type={type}
+        payload={payload}
+      >
         <div className={styles.blockItemContainer}>
           <IconFont
             style={{ fontSize: 20 }}
@@ -113,93 +120,4 @@ function BlockItem({
   );
 }
 
-function LayoutItem({
-  columns,
-  title,
-}: {
-  columns: string[][];
-  title: string;
-}) {
-  const [visible, setVisible] = useState(false);
 
-  return (
-    <div>
-      <p
-        onClick={() => setVisible((v) => !v)}
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          cursor: 'pointer',
-        }}
-      >
-        <span>{title}</span>
-        {columns.length > 1 && (
-          <span>{!visible ? <IconCaretRight /> : <IconCaretUp />}</span>
-        )}
-      </p>
-      {columns.map((item, index) => {
-        const hide = !visible && index !== 0;
-        const payload = {
-          type: AdvancedType.SECTION,
-          attributes: {},
-          children: item.map((col) => ({
-            type: AdvancedType.COLUMN,
-            attributes: {
-              width: col,
-            },
-            data: {
-              value: {},
-            },
-            children: [],
-          })),
-        };
-
-        return (
-          <div
-            key={index}
-            style={{
-              height: hide ? 0 : undefined,
-              overflow: 'hidden',
-              marginBottom: hide ? 0 : 20,
-            }}
-          >
-            <BlockAvatarWrapper type={AdvancedType.SECTION} payload={payload}>
-              <div
-                style={{
-                  border: '1px solid rgb(229, 229, 229)',
-                  width: '100%',
-                  padding: 10,
-                }}
-              >
-                <div
-                  style={{
-                    height: 16,
-                    border: '1px solid rgb(85, 85, 85)',
-                    borderRadius: 3,
-                    display: 'flex',
-                  }}
-                >
-                  {item.map((column, index) => {
-                    return (
-                      <div
-                        key={index}
-                        style={{
-                          borderRight:
-                            index === item.length - 1
-                              ? undefined
-                              : '1px solid rgb(85, 85, 85)',
-                          height: '100%',
-                          width: column,
-                        }}
-                      />
-                    );
-                  })}
-                </div>
-              </div>
-            </BlockAvatarWrapper>
-          </div>
-        );
-      })}
-    </div>
-  );
-}
