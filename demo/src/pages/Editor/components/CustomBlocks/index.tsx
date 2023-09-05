298c6cfd006eb1910ffa9a5c82b96b5646408285
kapilkumar009 <kapil.kumar@vikasit.com>
2023-09-05 10:30:10 +0530

    custom footer section added

--- demo/src/pages/Editor/components/CustomBlocks/index.tsx
+++ demo/src/pages/Editor/components/CustomBlocks/index.tsx
@@ -2,11 +2,18 @@ import { BlockManager } from 'vikasit-email-core';
 import { BlockAttributeConfigurationManager } from 'vikasit-email-extensions';
 import { CustomBlocksType } from './constants';
 import { Panel as ProductRecommendationPanel, ProductRecommendation } from './ProductRecommendation';
-
+import { FooterSection, FooterPanel as FooterRecmmendationPanel } from "./Footer/index"
 BlockManager.registerBlocks({
   [CustomBlocksType.PRODUCT_RECOMMENDATION]: ProductRecommendation,
 });
 
 BlockAttributeConfigurationManager.add({
   [CustomBlocksType.PRODUCT_RECOMMENDATION]: ProductRecommendationPanel,
+});
+
+BlockManager.registerBlocks({
+  [CustomBlocksType.FOOTER_SECTION]: FooterSection,
+});
+BlockAttributeConfigurationManager.add({
+  [CustomBlocksType.FOOTER_SECTION]: FooterRecmmendationPanel,
 });
